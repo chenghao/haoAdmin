25e4c5d8fded77e6a9a518a16c5fba519097452d
chenghao <hao2432@gmail.com>
2016-12-26 22:33:52 +0800

    11

--- startup.py
+++ startup.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 __author__ = "chenghao"
 
 from bottle import Bottle, static_file
@@ -12,20 +12,19 @@ bottle.merge(Routes)
 
 @bottle.get('/static/<filename:path>')
 def static(filename):
-	return static_file(filename, root='./static/')
+    return static_file(filename, root='./static/')
 
 
 @bottle.get('/upload/<filename:path>')
 def static(filename):
-	return static_file(filename, root='./upload/')
+    return static_file(filename, root='./upload/')
 
 
 app = LoginMiddleware(bottle, "/admin/*", ["/admin/login", "/admin/logout"])
 
+
 def dev_run():
-
     from bottle import run
-
     run(app=app, host="0.0.0.0", port=8000, reloader=True, debug=True)
 
 
